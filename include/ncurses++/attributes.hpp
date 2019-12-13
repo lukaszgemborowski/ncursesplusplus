946722d60f3c990d138e6e3f315a235e5d5c7ab1
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2019-12-13 20:55:37 +0100

    [draw] add attribute_on/off functions

---
+++ include/ncurses++/attributes.hpp
@@ -0,0 +1,21 @@
+#ifndef NCURSESPP_ATTRIBUTES_HPP
+#define NCURSESPP_ATTRIBUTES_HPP
+
+namespace ncursespp
+{
+
+enum class attribute
+{
+    normal,
+    standout,
+    underline,
+    reverse,
+    blink,
+    dim,
+    bold,
+    italic
+};
+
+} // namespace ncursespp
+
+#endif // NCURSESPP_ATTRIBUTES_HPP
--- include/ncurses++/drawing.hpp
+++ include/ncurses++/drawing.hpp
@@ -2,6 +2,7 @@
 #define NCURSESPP_DRAWING_HPP
 
 #include <ncurses++/rect.hpp>
+#include <ncurses++/attributes.hpp>
 
 namespace ncursespp
 {
@@ -10,6 +11,8 @@ struct draw
 {
     static void fill_rect(rect_i r, short color);
     static int text(const std::string &text, rect_i r, short color);
+    static void attribute_on(attribute attr);
+    static void attribute_off(attribute attr);
 };
 
 } // namespace ncursespp
