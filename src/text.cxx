be8df505334e0ddfd283ac41dccedf3285aba2a1
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2019-12-13 01:31:50 +0100

    text: implement multiline text support

--- src/text.cxx
+++ src/text.cxx
@@ -7,15 +7,32 @@ namespace ncursespp
 
 void text::do_resize(rect_i r)
 {
-    auto lt = r.left_top;
-    auto width = r.width();
-
-    attron(COLOR_PAIR(color_));
-
     draw::fill_rect(r, color_);
 
-    for (auto x = 0; x < width && static_cast<std::size_t>(x) < text_.size(); ++x) {
-        mvaddch(lt.y, x + lt.x, text_[x]);
+    auto p = r.left_top;
+    auto i = 0u;
+    attron(COLOR_PAIR(color_));
+
+    while (text_[i] != '\0') {
+        if (p.y > r.right_bottom.y) {
+            break;
+        }
+
+        if (text_[i] == '\n') {
+            p.x = r.left_top.x;
+            p.y ++;
+            i ++;
+            continue;
+        }
+
+        if (p.x > r.right_bottom.x) {
+            i ++;
+            continue;
+        }
+
+        mvaddch(p.y, p.x, text_[i]);
+        i ++;
+        p.x ++;
     }
 
     attroff(COLOR_PAIR(color_));
